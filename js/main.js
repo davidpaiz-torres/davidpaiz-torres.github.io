9b06daf6e28625190daa31a6e8eea9404ea81082
dpt82 <143633654+dpt82@users.noreply.github.com>
2023-12-22 23:55:43 -0500

    design overhaul
    Implemented more css and javascript.
    Github Co-Pilot was used to help implement javascript as this was my first time coding with javascript. All design ideas and choices came from  me.

---
+++ js/main.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('resume_button').addEventListener('click', function(event) {
+        var confirmLeave = window.confirm("You are about to leave to another page. Are you sure?");
+        if (!confirmLeave) {
+            event.preventDefault();
+        }
+    });
+
+    var image = document.getElementById('adams_event');
+    var caption = document.querySelector('.images figcaption');
+    var charts = document.querySelector('.charts');
+    var map = document.querySelector('.map');
+    var screenHeight = window.innerHeight;
+
+    window.addEventListener('scroll', function() {
+        var imageMiddle = image ? image.getBoundingClientRect().top + image.offsetHeight / 2 : 0;
+        var captionMiddle = caption ? caption.getBoundingClientRect().top + caption.offsetHeight / 2 : 0;
+        var chartsMiddle = charts ? charts.getBoundingClientRect().top + charts.offsetHeight / 2 : 0;
+        var mapMiddle = map ? map.getBoundingClientRect().top + map.offsetHeight / 2 : 0;
+
+        image.style.opacity = Math.max(0, Math.min(1, 1 - Math.abs(screenHeight / 2 - imageMiddle) / (screenHeight / 3)));
+        caption.style.opacity = Math.max(0, Math.min(1, 1 - Math.abs(screenHeight / 2 - captionMiddle) / (screenHeight / 3)));
+        charts.style.opacity = Math.max(0, Math.min(1, 1 - Math.abs(screenHeight / 2 - chartsMiddle) / (screenHeight / 3)));
+        map.style.opacity = Math.max(0, Math.min(1, 1 - Math.abs(screenHeight / 2 - mapMiddle) / (screenHeight / 3)));
+    });
+});
