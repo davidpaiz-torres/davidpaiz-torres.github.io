50d8239cda9608cb83dd95a7de40a4e64cb70c52
dpt82 <143633654+dpt82@users.noreply.github.com>
2024-01-31 23:17:03 -0500

    Update alert.js

--- js/alert.js
+++ js/alert.js
@@ -1,10 +1,18 @@
-fetch('projects.json')
-    .then(response => response.json())
-    .then(projects => {
-        var projectsDiv = document.getElementById('projects');
-        projects.forEach(project => {
-            var projectDiv = document.createElement('div');
-            projectDiv.textContent = project.name;
-            projectsDiv.appendChild(projectDiv);
+document.addEventListener('DOMContentLoaded', function() {
+    ['resume', 'github'].forEach(function(buttonId) {
+        document.getElementById(buttonId).addEventListener('click', function(event) {
+            var confirmLeave = window.confirm("You are about to leave to another page. Are you sure?");
+            if (!confirmLeave) {
+                event.preventDefault();
+            }
         });
     });
+   
+   document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('resume_button').addEventListener('click', function(event) {
+        var confirmLeave = window.confirm("You are about to leave to another page. Are you sure?");
+        if (!confirmLeave) {
+            event.preventDefault();
+        }
+    }); 
+   });});
