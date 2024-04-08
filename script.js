bc6b54c5ea6266d17ac977912ba48ff3cb437c74
dpt82 <143633654+dpt82@users.noreply.github.com>
2024-04-07 20:43:48 -0400

    ALMOST THERE

--- script.js
+++ script.js
@@ -1,39 +1,37 @@
+const text_change = [
+  'THE HARDSHIPS OF CONGOLESE MIGRANTS',
+  'FINDING COMMUNITY IN HARLEM',
+  'A TASTE OF HOME'
+];
+
 const updateContent = (index) => {
   // Update content based on the current index
   console.log(text_change[index]);
 
-  // Update image visibility based on the media query conditions
-  const mediaQuery = window.matchMedia('(max-width: 600px) and (orientation: portrait)');
+  // Update image based on the current index
   const imageElements = [
     document.getElementById('hardships'),
     document.getElementById('person'),
     document.getElementById('bags')
   ];
 
-  if (mediaQuery.matches) {
-    // Small screen in portrait orientation
-    imageElements.forEach((imageElement, i) => {
-      imageElement.style.display = i === index ? 'block' : 'none';
-    });
-  } else {
-    // Larger screens or other orientations
-    imageElements.forEach((imageElement) => {
-      imageElement.style.display = 'block'; // Show all images
-    });
-  }
-};
+  // imageElements.forEach((imageElement, i) => {
+  //   if (i === index) {
+  //     imageElement.style.display = 'block'; // Show the image
+  //   } else {
+  //     imageElement.style.display = 'none'; // Hide other images
+  //   }
+  };
+
+const images = document.querySelectorAll('.image-container img');
 
 const scroller = new Scroller({
   scenes: document.querySelectorAll('.scene'),
   container: document.querySelector('.container'),
+  rootMargin: '-850px 0px', 
+  threshold: 0 
 });
 
-let options = {
-  root: document.querySelector("#scrollArea"),
-  rootMargin: "0px",
-  threshold: 1.0,
-};
-let observer = new IntersectionObserver(callback, options);
 // Scroller has a tiny event emitter embedded in it!
 
 // the `enter` event is triggered every time a scene crosses the threshold
