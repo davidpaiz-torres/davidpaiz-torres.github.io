143117f5e66f30913786173f6cc4ff8121fef247
dpt82 <143633654+dpt82@users.noreply.github.com>
2024-04-15 00:30:10 -0400

    added flex

--- scripts/script.js
+++ scripts/script.js
@@ -11,7 +11,7 @@ function promptForDistrict() {
     console.log(userInput); // Log the inputted data to the console
 
     if (userInput) {
-        // Check if user input is a number
+// Check if user input is a number (will need to rethink this if this ever expands to districts outside of NY)
         if (isNaN(userInput)) {
             alert("Please Enter an NYC Congressional District!");
             // Retrigger the prompt 
@@ -19,7 +19,7 @@ function promptForDistrict() {
             return;
         }
 
-        // Check if user input is a valid district
+// Check if user input is a valid district
         if (!validDistricts.includes(parseInt(userInput))) {
             alert("Invalid District! Enter an NYC Congressional District");
             // Retrigger the prompt 
@@ -27,11 +27,11 @@ function promptForDistrict() {
             return;
         }
 
-        // Create a pop-up so the user knows the district is loading.
+// Create a pop-up so the user knows the district is loading.
         const popup = document.createElement("div");
         popup.textContent = "Loading District Information";
         popup.style.position = "fixed";
-        popup.style.top = "26%";
+        popup.style.top = "26%"; // Set the pop up slightly above the capitol 
         popup.style.left = "50%";
         popup.style.transform = "translate(-50%, -50%)";
         popup.style.padding = "10px";
@@ -57,3 +57,4 @@ function launchNewPage(districtNumber) {
     // Navigate to the new page
     window.location.href = url;
 }
+
