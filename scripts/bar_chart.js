f0d28b8cd6777b92af16334d577652db7d041905
davidpaiz-torres <david.paiz-torres82@journalism.cuny.edu>
2024-11-17 17:59:37 -0500

    Update bar_chart.js

--- scripts/bar_chart.js
+++ scripts/bar_chart.js
@@ -7,8 +7,7 @@ const bar_margin = {top: 40, right: 60, bottom: 60, left: 60},
       .attr("height", bar_height + bar_margin.top + bar_margin.bottom)
       .append("g")
       .attr("transform", `translate(${bar_margin.left},${bar_margin.top})`); 1 
-
-// Load data
-d3.csv("data/arrests_by_demographics.csv").then(data => {
-
-});
+      
+      d3.csv("data/arrests_by_demographics.csv").then(data => {
+        
+      });
